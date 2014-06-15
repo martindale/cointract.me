f93eb8102da383171981b8083749be90e16fde09
Gordon Hall <gordon@gordonwritescode.com>
2014-06-15 14:09:47 -0400

    added tests for User.create(), added env var for TEST to not print messages to the console, changed profile.username to username

--- tests/models/user.unit.js
+++ tests/models/user.unit.js
@@ -6,12 +6,51 @@ mockgoose(mongoose);
 
 describe('User', function () {
 
-  var User = require('../../lib/models/user');
+  var User   = require('../../lib/models/user');
+  var crypto = require('crypto');
 
   describe('#create', function() {
+
     it('should create the user', function(done) {
-      done();
-      // TODO - write real tests ;)
+      User.create({
+        email: 'valid@email.com',
+        password: crypto.createHash('sha1').update('password').digest('hex'),
+        username: 'testuser'
+      }, done);
     });
+
+    it('should fail with existing email', function(done) {
+      User.create({
+        email: 'valid@email.com',
+        password: crypto.createHash('sha1').update('password').digest('hex'),
+        username: 'testuser2'
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should fail with duplicate username', function(done) {
+      User.create({
+        email: 'another@email.com',
+        password: crypto.createHash('sha1').update('password').digest('hex'),
+        username: 'testuser'
+      }, function(err, user) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should fail with no password', function(done) {
+      User.create({
+        email: 'another@email.com',
+        username: 'testuser2'
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+
   });
+
 });
