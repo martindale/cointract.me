da73967a6ebcb0c6d5e13499cd188731e6c5b1d5
Gordon Hall <gordon@gordonwritescode.com>
2014-06-16 15:40:07 -0400

    finish job model tests, fix bugs in job model

--- tests/models/job.unit.js
+++ tests/models/job.unit.js
@@ -1,8 +1,8 @@
 var mongoose   = require('mongoose');
 var mockgoose  = require('mockgoose');
 var should     = require('should');
-var proxyquire = require('proxyquire');
-var sinon      = require('sinon');
+var qs         = require('querystring');
+var url        = require('url');
 
 mockgoose(mongoose);
 
@@ -10,24 +10,84 @@ describe('Job', function() {
 
   var Job   = require('../../lib/models/job');
   var user1 = { _id: new mongoose.Types.ObjectId() };
+  var user2 = { _id: new mongoose.Types.ObjectId() };
   var job   = null;
+
+  var bid = {
+    _id: new mongoose.Types.ObjectId(),
+    rate: 0.25e8,
+    pricingType: 'hourly',
+    user: user1._id
+  };
+
+  var tags = ['bitcoin','javascript','open source'];
 
   describe('#create', function() {
 
     it('should fail with invalid title', function(done) {
-
+      Job.create(user2, {
+        title: null,
+        rate: 1e8,
+        pricingType: 'fixed',
+        category: 'Programming',
+        tags: tags
+      }, function(err, result) {
+        should.exist(err);
+        should.not.exist(result);
+        done();
+      });
     });
 
     it('should fail with invalid category', function(done) {
-
+      Job.create(user2, {
+        title: 'Fix My Crappy Software!',
+        rate: 1e8,
+        pricingType: 'fixed',
+        category: 'Computers',
+        tags: tags
+      }, function(err, result) {
+        should.exist(err);
+        should.not.exist(result);
+        done();
+      });
     });
 
     it('should fail with invalid subcategory', function(done) {
-
+      Job.create(user2, {
+        title: 'Fix My Crappy Software!',
+        rate: 1e8,
+        pricingType: 'fixed',
+        category: 'Programming',
+        subCategory: 'Internets',
+        tags: tags
+      }, function(err, result) {
+        should.exist(err);
+        should.not.exist(result);
+        done();
+      });
     });
 
     it('should create the job', function(done) {
-
+      Job.create(user2, {
+        title: 'Fix My Crappy Software!',
+        rate: 1e8,
+        pricingType: 'fixed',
+        category: 'Programming',
+        subCategory: 'Software',
+        tags: tags
+      }, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.title.should.equal('Fix My Crappy Software!');
+        result.rate.should.equal(1e8);
+        result.pricingType.should.equal('fixed');
+        result.category.should.equal('Programming');
+        result.subCategory.should.equal('Software');
+        result.tags.should.instanceOf(Array).and.have.lengthOf(3);
+        job     = result;
+        bid.job = result._id
+        done();
+      });
     });
 
   });
@@ -35,15 +95,44 @@ describe('Job', function() {
   describe('#update', function() {
 
     it('should update the job', function(done) {
-
+      job.update({
+        title: 'PLEASE fix my crappy software!',
+        description: 'My nephew built my app, call him for more information',
+        subCategory: 'Mobile'
+      }, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.title.should.equal('PLEASE fix my crappy software!');
+        should.exist(result.description);
+        result.subCategory.should.equal('Mobile');
+        done();
+      });
     });
 
   });
 
-  describe('#queueForPublihsing', function() {
+  describe('#queueForPublishing', function() {
+
+    var start = new Date('2022-06-01').toString();
+    var end   = new Date('2022-06-08').toString();
+
+    it('should not publish the job with invalid dates', function(done) {
+      job.queueForPublishing(end, start, function(err, result) {
+        should.exist(err);
+        done();
+      });
+    });
 
     it('should publish the job', function(done) {
-
+      job.queueForPublishing(start, end, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.listing.start.toString().should.equal(start);
+        result.listing.end.toString().should.equal(end);
+        should.exist(result.listing.activationCode);
+        result.listing.active.should.equal(true);
+        done();
+      });
     });
 
   });
@@ -51,7 +140,11 @@ describe('Job', function() {
   describe('#getPublishURL', function() {
 
     it('should return a url string for bitpay ipn', function(done) {
-
+      var publishUrl = job.getPublishURL();
+      qs.parse(url.parse(publishUrl).query).activationCode.should.equal(
+        job.listing.activationCode
+      );
+      done();
     });
 
   });
@@ -59,7 +152,11 @@ describe('Job', function() {
   describe('#watch', function() {
 
     it('should add user to watchlist', function(done) {
-
+      job.watch(user1, function(err) {
+        should.not.exist(err);
+        job.watchers.should.instanceOf(Array).and.have.lengthOf(1);
+        done();
+      });
     });
 
   });
@@ -67,7 +164,11 @@ describe('Job', function() {
   describe('#unwatch', function() {
 
     it('should remove user from watchlist', function(done) {
-
+      job.unwatch(user1, function(err) {
+        should.not.exist(err);
+        job.watchers.should.instanceOf(Array).and.have.lengthOf(0);
+        done();
+      });
     });
 
   });
@@ -83,7 +184,14 @@ describe('Job', function() {
   describe('#acceptBid', function() {
 
     it('should add contractor and set job status to open', function(done) {
-
+      job.acceptBid(bid, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.rate.should.equal(bid.rate);
+        result.pricingType.should.equal(bid.pricingType);
+        result.contractor.should.equal(bid.user);
+        done();
+      });
     });
 
   });
