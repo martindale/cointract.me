354a58d68b5f108556c721b749838984d63f3162
Gordon Hall <gordon@gordonwritescode.com>
2014-06-15 21:26:16 -0400

    added rating model tests, and fixed some rating model bugs

---
+++ tests/models/rating.unit.js
@@ -0,0 +1,49 @@
+var mongoose   = require('mongoose');
+var mockgoose  = require('mockgoose');
+var should     = require('should');
+
+mockgoose(mongoose);
+
+describe('Rating', function() {
+
+  var Rating = require('../../lib/models/rating');
+  var rating = null;
+
+  var critic = new mongoose.Types.ObjectId();
+  var userId = new mongoose.Types.ObjectId();
+
+  describe('#create', function() {
+
+    it('should create a new rating', function(done) {
+      Rating.create({ _id: critic }, {
+        user: userId,
+        score: 8
+      }, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.score.should.equal(8);
+        rating = result;
+        done();
+      });
+    });
+
+  });
+
+  describe('#update', function() {
+
+    it('should update the rating', function(done) {
+      rating.update({
+        score: 10,
+        review: 'Great work!'
+      }, function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.score.should.equal(10);
+        result.review.should.equal('Great work!');
+        done();
+      });
+    });
+
+  });
+
+});
