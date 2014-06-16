56b0fefe6ccb090304b3c6b2ba74d898fc1c94ad
Gordon Hall <gordon@gordonwritescode.com>
2014-06-16 14:55:32 -0400

    removed extraneous methods from job model and scaffolded job model tests

--- tests/models/job.unit.js
+++ tests/models/job.unit.js
@@ -14,8 +14,76 @@ describe('Job', function() {
 
   describe('#create', function() {
 
-    it('', function(done) {
-      
+    it('should fail with invalid title', function(done) {
+
+    });
+
+    it('should fail with invalid category', function(done) {
+
+    });
+
+    it('should fail with invalid subcategory', function(done) {
+
+    });
+
+    it('should create the job', function(done) {
+
+    });
+
+  });
+
+  describe('#update', function() {
+
+    it('should update the job', function(done) {
+
+    });
+
+  });
+
+  describe('#queueForPublihsing', function() {
+
+    it('should publish the job', function(done) {
+
+    });
+
+  });
+
+  describe('#getPublishURL', function() {
+
+    it('should return a url string for bitpay ipn', function(done) {
+
+    });
+
+  });
+
+  describe('#watch', function() {
+
+    it('should add user to watchlist', function(done) {
+
+    });
+
+  });
+
+  describe('#unwatch', function() {
+
+    it('should remove user from watchlist', function(done) {
+
+    });
+
+  });
+
+  describe('#notifyHired', function() {
+
+    it.skip('should send email notification on accepted bid', function(done) {
+
+    });
+
+  });
+
+  describe('#acceptBid', function() {
+
+    it('should add contractor and set job status to open', function(done) {
+
     });
 
   });
